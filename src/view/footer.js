ebc13e7bca997677e09814952ff3030e520bb342
n7best <n7best@gmail.com>
2017-10-04 11:11:20 -0400

    page

--- src/view/footer.js
+++
@@ -1,8 +0,0 @@
-import { h } from '../utils/engine'
-import { MoveOnScroll, moveOnScrollOpacity, showOnSeen } from '../hoc/moveOnScroll'
-
-export const footer = container => {
-
-    return h('div', 'footer', 'Copyright 2016 Charlie Lin')
-
-}
--- src/view/intro.js
+++
@@ -1,7 +0,0 @@
-import { h } from '../utils/engine'
-//intro components
-export const intro_text = h('span', '', 'Hi, there!')
-
-export const intro = h('div', 'intro', intro_text)
-export const logo = h('img', 'logo')
-logo.src = 'assets/img/logo.png'
